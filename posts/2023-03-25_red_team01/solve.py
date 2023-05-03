64f35ab99af9c18fc7cb5b82b3a44ec8b4af4c69
trevorsaudi <trevorsaudi@users.noreply.github.com>
2023-05-03 12:04:38 +0000

    deploy: 2433bcc0f1a7d12e442a40085020604ad7094509

--- posts/2023-03-25_red_team01/solve.py
+++ posts/2023-03-25_red_team01/solve.py
@@ -1,26 +1,14 @@
-enc_flag = [91,241,101,166,85,192,87,188,110,164,99,152,98,252,34,152,117,164,99,162,107]
-flag_len = len(enc_flag)
+flag_len = 21
+enc_flag = [91, 241, 101, 166, 85, 192, 87, 188, 110, 164, 99, 152, 98, 252, 34, 152, 117, 164, 99, 162, 107]
 key_len = 4
-found = False
-
-for k1 in range(256):
-    for k2 in range(256):
-        for k3 in range(256):
-            for k4 in range(256):
-                key = [k1, k2, k3, k4]
-                flag = ""
-                for i in range(flag_len):
-                    c = chr(enc_flag[i] ^ key[i % key_len])
-                    flag += c
-                print(flag)
-                if flag.startswith("RS{") and flag.endswith("}"):
-                    found = True
-                    print("Found key:", key)
-                    print("Flag:", flag)
-                    break
-            if found:
-                break
-        if found:
-            break
-    if found:
-        break
+enc_flag = "5bf165a655c057bc6ea4639862fc229875a463a26b"
+# Brute force the key by looping through all possible values
+for i in range(10):
+    for j in range(10):
+        for k in range(10):
+            for l in range(10):
+                key = [i, j, k, l]
+                decrypted_flag = ""
+                for idx, c in enumerate(enc_flag):
+                    decrypted_flag += (int(c) ^ key[idx % key_len])
+                    print(decrypted_flag)
